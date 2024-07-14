7c3e499001c394f295c15013705fd6c4f41da962
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2024-07-14 05:41:32 +0500

    fix the issue

--- src/pages/home/components/workProcess/WorkProcess.tsx
+++ src/pages/home/components/workProcess/WorkProcess.tsx
@@ -9,6 +9,7 @@ import { AccordionTitle, AccordionPanel } from '@src/components/ui';
 
 export const WorkProcess = () => {
   const [activeId, setActiveId] = useState<number>(0);
+
   const handleToggle = (id: number) => {
     setActiveId((prevId) => (prevId === id ? 0 : id));
   };
@@ -28,34 +29,52 @@ export const WorkProcess = () => {
         className='absolute top-0'
       />
       <div className='mx-auto max-w-screen-xl py-36'>
-        <div>{sectionHeading.title}</div>
-        <h1 className='py-8 text-5xl font-bold'>
-          Our <span className='text-[#0044EB]'>Approach</span>
-        </h1>
-        {workProcessData?.map((item) => {
-          const isOpen = activeId === item.id;
-          return (
-            <Fragment key={item.id}>
-              <AccordionTitle
-                id={item.id}
-                handleToggle={handleToggle}
-                className='grid grid-cols-2'
-              >
-                <div className='flex justify-between border-b border-[#CCE3FF] py-4 text-xl'>
-                  {item.no}. {item.title}
-                  {isOpen ? <Minus /> : <Plus />}
-                </div>
-                <div className='bg-slate-400'>{item.title}</div>
-              </AccordionTitle>
-              <AccordionPanel
-                isOpen={activeId === item.id}
-                className='w-1/2 bg-[#CCE3FF] p-5'
-              >
-                {item.des}
-              </AccordionPanel>
-            </Fragment>
-          );
-        })}
+        <div className='flex items-center gap-10'>
+          <div className='w-1/2'>
+            <div>{sectionHeading.title}</div>
+            <h1 className='py-8 text-5xl font-bold'>
+              Our <span className='text-[#0044EB]'>Approach</span>
+            </h1>
+            {workProcessData?.map((item: any) => {
+              const isOpen = activeId === item.id;
+              return (
+                <Fragment key={item.id}>
+                  <AccordionTitle
+                    id={item.id}
+                    handleToggle={handleToggle}
+                    className='flex justify-between border-b border-[#CCE3FF] py-4 text-xl font-bold hover:text-[#0044EB]'
+                  >
+                    {item.no}. {item.title}
+                    {isOpen ? <Minus /> : <Plus />}
+                  </AccordionTitle>
+                  <AccordionPanel
+                    isOpen={activeId === item.id}
+                    className='bg-[#CCE3FF] p-5'
+                  >
+                    {item.des}
+                  </AccordionPanel>
+                </Fragment>
+              );
+            })}
+          </div>
+          <div className='flex w-1/2 flex-col items-center justify-center'>
+            {workProcessData?.map((item: any) => {
+              return (
+                <button
+                  onClick={() => handleToggle(item.id)}
+                  key={item.id}
+                  className={`-mb-5 h-36 w-[450px] rounded-[100%] text-2xl ${
+                    activeId === item.id
+                      ? 'z-30 bg-[#0044EB] text-white'
+                      : 'z-10 border border-[#020842] bg-transparent text-[#020842]'
+                  }`}
+                >
+                  {item.title}
+                </button>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
