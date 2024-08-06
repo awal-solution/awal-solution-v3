997106bb920d656479f5d1e7bc45795365546942
umer hayat <iummerhayat@gmail.com>
2024-08-06 15:52:12 +0500

    created contact us page

---
+++ src/pages/contact/ContactUs.tsx
@@ -0,0 +1,193 @@
+
+const ContactUs = () => {
+  return (<>
+    <div>
+      <div className="relative">
+        <img className="relative  w-full" src="/src/assets/images/bg_pattern_4.svg" alt="" />
+        <div className="absolute inset-0 h-96 bg-[#020842e6]">
+          <div className="mx-64">
+            <div className="pt-32 text-center text-white">
+              <span className="bg-pink-400"> contact </span>
+              us 😍
+            </div>
+            <h1 className="pt-5 text-center text-6xl font-bold text-white">
+              Contact Us
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#E3F0FF] pb-20 pt-40">
+        <div className="mx-64">
+          <div className="grid grid-cols-4 gap-6">
+            {/* First Card */}
+            <div className="rounded-2xl bg-white px-3 py-16 duration-300 ease-linear hover:-translate-y-1 hover:shadow-xl">
+              <div className="mx-auto flex size-16 items-center justify-center rounded-full bg-[#0044eb1a] text-4xl text-[#0044EB]">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M12 11.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7"
+                  />
+                </svg>
+              </div>
+              <div className="py-2 text-center text-[#020842]">
+                <strong>Location </strong>
+              </div>
+              <div className="px-6">
+                <p className="pt-2 text-center font-medium text-[#49515B]">
+                  Sunshine Business Park Sector-
+                </p>
+              </div>
+              <p className="text-center text-lg font-medium text-[#49515B]">
+                94, Poland
+              </p>
+            </div>
+            {/* Second Card */}
+            <div className="rounded-2xl bg-white px-3 py-16 duration-300 ease-linear hover:-translate-y-1 hover:shadow-xl">
+              <div className="mx-auto flex size-16 items-center justify-center rounded-full bg-[#0044eb1a] text-4xl text-[#0044EB]">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256">
+                  <path
+                    fill="currentColor"
+                    d="M144.27 45.93a8 8 0 0 1 9.8-5.66a86.22 86.22 0 0 1 61.66 61.66a8 8 0 0 1-5.66 9.8a8.2 8.2 0 0 1-2.07.27a8 8 0 0 1-7.73-5.93a70.35 70.35 0 0 0-50.33-50.34a8 8 0 0 1-5.67-9.8m-2.33 41.8c13.79 3.68 22.65 12.55 26.33 26.34A8 8 0 0 0 176 120a8.2 8.2 0 0 0 2.07-.27a8 8 0 0 0 5.66-9.8c-5.12-19.16-18.5-32.54-37.66-37.66a8 8 0 1 0-4.13 15.46m72.43 78.73l-47.11-21.11l-.13-.06a16 16 0 0 0-15.17 1.4a8 8 0 0 0-.75.56L126.87 168c-15.42-7.49-31.34-23.29-38.83-38.51l20.78-24.71c.2-.25.39-.5.57-.77a16 16 0 0 0 1.32-15.06v-.12L89.54 41.64a16 16 0 0 0-16.62-9.52A56.26 56.26 0 0 0 24 88c0 79.4 64.6 144 144 144a56.26 56.26 0 0 0 55.88-48.92a16 16 0 0 0-9.51-16.62"
+                  />
+                </svg>
+              </div>
+              <div className="py-2 text-center text-[#020842]">
+                <strong>Contact </strong>
+              </div>
+              <div className="px-6">
+                <p className="pt-2 text-center font-medium text-[#49515B]">
+                  +88(0) 555-0108
+                </p>
+              </div>
+              <p className="text-center text-lg font-medium text-[#49515B]">
+                +88(0) 555-01117
+              </p>
+            </div>
+            {/* Third Card */}
+            <div className="rounded-2xl bg-white px-3 py-16 duration-300 ease-linear hover:-translate-y-1 hover:shadow-xl">
+              <div className="mx-auto flex size-16 items-center justify-center rounded-full bg-[#0044eb1a] text-4xl text-[#0044EB]">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m0 4l-8 5l-8-5V6l8 5l8-5z"
+                  />
+                </svg>
+              </div>
+              <div className="py-2 text-center text-[#020842]">
+                <strong>Email </strong>
+              </div>
+              <div className="px-6">
+                <p className="pt-2 text-center font-medium text-[#49515B]">
+                  Techco@example.com
+                </p>
+              </div>
+              <p className="text-center text-lg font-medium text-[#49515B]">
+                gmail.@example.com
+              </p>
+            </div>
+            {/* Fourth Card */}
+            <div className="rounded-2xl bg-white px-3 py-16 duration-300 ease-linear hover:-translate-y-1 hover:shadow-xl">
+              <div className="mx-auto flex size-16 items-center justify-center rounded-full bg-[#0044eb1a] text-4xl text-[#0044EB]">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M12 11.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7"
+                  />
+                </svg>
+              </div>
+              <div className="py-2 text-center text-[#020842]">
+                <strong>Visit Between </strong>
+              </div>
+              <div className="px-6">
+                <p className="pt-2 text-center font-medium text-[#49515B]">
+                  Mon - Sat: 8.00-5.00
+                </p>
+              </div>
+              <p className="text-center text-lg font-medium text-[#49515B]">
+                Sunday: Closed
+              </p>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-80 pt-24">
+            <div>
+              <h2 className="mb-2 font-serif text-5xl font-bold text-[#020842]">
+                Send Us A Message
+              </h2>
+              <p className="pb-11 font-serif text-lg text-[#49515B]">
+                Give us chance to serve and bring magic to your brand.
+              </p>
+              <div className="flex gap-3 pb-3">
+                <div>
+                  <label className="pb-3 text-base font-semibold" htmlFor="input_name">Full Name</label>
+                  <div className="p-2">
+                    <input
+                      className="w-[350px] rounded-xl px-2 py-4 outline-blue-700"
+                      type="text"
+                      placeholder="Goladra Gomaz"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label className="pb-3 text-base font-semibold" htmlFor="input_email">Your Email</label>
+                  <div className="p-2">
+                    <input
+                      className="w-[350px] rounded-xl px-2 py-4 outline-blue-700"
+                      type="text"
+                      placeholder="Techco@example.com"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <label className="pb-3 text-base font-semibold" htmlFor="input_phone">Your Phone</label>
+                <div className="p-2">
+                  <input
+                    className="w-[730px] rounded-xl px-2 py-4 outline-blue-700"
+                    type="text"
+                    placeholder="+8250-3560 6565"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="pb-3 text-base font-semibold" htmlFor="input_message">Message</label>
+                <div className="px-2 pb-4 pt-2">
+                  <textarea
+                    className="w-[730px] rounded-xl px-3 py-2 outline-blue-700"
+                 
+                    id="input_message"
+                    placeholder="How can we help you?"
+                  ></textarea>
+                </div>
+                <button className="flex items-center justify-center rounded-full bg-blue-700 px-16 py-5 pt-6 text-base text-white">
+                  <div>SEND MESSAGE</div>
+                  <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                      <path
+                        fill="white"
+                        d="m5.99 16.596l8.192-8.192H7.818v-2h9.778v9.778h-2V9.818L7.403 18.01z"
+                      />
+                    </svg>
+                  </div>
+                </button>
+              </div>
+            </div>
+            <div>
+              <iframe
+                className="rounded-3xl"
+                src="https://maps.google.com/maps?q=London%20Eye%2C%20London%2C%20United%20Kingdom&t=m&z=10&output=embed&iwloc=near"
+                width="500"
+                height="600"
+                style={{ border: 0 }}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      </>
+  );
+};
+
+export default ContactUs;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { AppLayout } from '@src/layouts/AppLayout';
 // Pages
 import { Home } from '@src/pages/home/Home';
 import { NotFound } from '@src/pages/404/NotFound';
+import ContactUs from '@src/pages/contact/ContactUs';
 // import { SingleProject } from '@src/pages/singleProject/SingleProject';
 
 export const Router = () => {
@@ -11,6 +12,7 @@ export const Router = () => {
       <Route path='/' element={<AppLayout />}>
         <Route index element={<Home />} />
         <Route path='home' element={<Home />} />
+        <Route path='contact' element={<ContactUs />} />
         {/* <Route path='portfolio/:projectName' element={<SingleProject />} /> */}
         <Route path='*' element={<NotFound />} />
       </Route>
