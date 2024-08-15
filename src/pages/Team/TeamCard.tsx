82c745b87392bea9585362d718f58bc2c0f86a20
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2024-08-15 17:24:34 +0500

    fix issues

--- src/pages/Team/TeamCard.tsx
+++ src/pages/Team/TeamCard.tsx
@@ -1,35 +1,41 @@
 /* eslint-disable tailwindcss/migration-from-tailwind-2 */
-import { ArrowUpRight, Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
+import {
+  ArrowUpRight,
+  Facebook,
+  Instagram,
+  Linkedin,
+  Twitter,
+} from 'lucide-react';
 
 export const TeamCard = ({ srcImg, name, post }: any) => {
-    return (
-        <div className="relative mx-5 rounded-3xl bg-[#E3F0FF] p-5 sm:mx-auto sm:w-fit md:mx-auto md:w-fit lg:w-fit">
-            <div className="group relative overflow-hidden rounded-3xl bg-white">
-                <img
-                    src={srcImg}
-                    alt="Team member"
-                    className="h-auto w-full object-cover transition-all duration-300 ease-linear hover:blur-[1px] sm:h-[300px] md:h-[400px]"
-                />
-                <div className="absolute inset-0 mx-auto mt-[40%] flex h-fit w-fit  p-16 items-center justify-center rounded-full bg-[#0044EB] bg-opacity-0 transition-all duration-300 ease-linear group-hover:bg-opacity-100">
-                    <ArrowUpRight className="text-white opacity-0 transition-all duration-300 ease-linear group-hover:opacity-100" />
-                </div>
-            </div>
-            <h1 className="pt-5 text-center text-3xl font-bold">{name}</h1>
-            <p className="pt-2 text-center font-semibold">{post}</p>
-            <div className="flex justify-center gap-3 pt-3">
-                <div className="group rounded bg-white p-2 hover:bg-blue-500">
-                    <Facebook className="text-gray-500 group-hover:text-white" />
-                </div>
-                <div className="group rounded bg-white p-2 hover:bg-blue-500">
-                    <Twitter className="text-gray-500 group-hover:text-white" />
-                </div>
-                <div className="group rounded bg-white p-2 hover:bg-blue-500">
-                    <Linkedin className="text-gray-500 group-hover:text-white" />
-                </div>
-                <div className="group rounded bg-white p-2 transition-all duration-300 ease-linear hover:bg-blue-500">
-                    <Instagram className="text-gray-500 group-hover:text-white" />
-                </div>
-            </div>
+  return (
+    <div className='relative mx-5 rounded-3xl bg-[#E3F0FF] p-5 sm:mx-auto sm:w-fit md:mx-auto md:w-fit lg:w-fit'>
+      <div className='group relative overflow-hidden rounded-3xl bg-white'>
+        <img
+          src={srcImg}
+          alt='Team member'
+          className='h-auto w-full object-cover transition-all duration-300 ease-linear hover:blur-[1px] sm:h-[300px] md:h-[400px]'
+        />
+        <div className='absolute inset-0 mx-auto mt-[40%] flex size-fit items-center  justify-center rounded-full bg-[#0044EB] bg-opacity-0 p-16 transition-all duration-300 ease-linear group-hover:bg-opacity-100'>
+          <ArrowUpRight className='text-white opacity-0 transition-all duration-300 ease-linear group-hover:opacity-100' />
         </div>
-    );
+      </div>
+      <h1 className='pt-5 text-center text-3xl font-bold'>{name}</h1>
+      <p className='pt-2 text-center font-semibold'>{post}</p>
+      <div className='flex justify-center gap-3 pt-3'>
+        <div className='group rounded bg-white p-2 hover:bg-blue-500'>
+          <Facebook className='text-gray-500 group-hover:text-white' />
+        </div>
+        <div className='group rounded bg-white p-2 hover:bg-blue-500'>
+          <Twitter className='text-gray-500 group-hover:text-white' />
+        </div>
+        <div className='group rounded bg-white p-2 hover:bg-blue-500'>
+          <Linkedin className='text-gray-500 group-hover:text-white' />
+        </div>
+        <div className='group rounded bg-white p-2 transition-all duration-300 ease-linear hover:bg-blue-500'>
+          <Instagram className='text-gray-500 group-hover:text-white' />
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/Team/TeamDetails.tsx
+++ src/pages/Team/TeamDetails.tsx
@@ -1,57 +1,56 @@
-import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
-import React from "react";
+import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react';
 
 const TeamDetail = () => {
   return (
-    <div className="bg-[#E3F0FF] p-10">
-      <div className="bg-white mt-32 p-6 rounded-3xl  flex gap-28    ">
-        <div className="rounded-3xl bg-[#E3F0FF]">
+    <div className='bg-[#E3F0FF] p-10'>
+      <div className='mt-32 flex gap-28 rounded-3xl  bg-white p-6'>
+        <div className='rounded-3xl bg-[#E3F0FF]'>
           <img
-            className="  w-[500px] h-[450px] object-contain"
-            src="https://wp.xpressbuddy.com/techco/wp-content/uploads/2024/05/team_member_image_3.webp"
-            alt="Profile"
+            className='h-[450px] w-[500px] object-contain'
+            src='https://wp.xpressbuddy.com/techco/wp-content/uploads/2024/05/team_member_image_3.webp'
+            alt='Profile'
           />
         </div>
         <div>
-          <h2 className="text-4xl font-bold text-black pt-16">
+          <h2 className='pt-16 text-4xl font-bold text-black'>
             August Everest
           </h2>
-          <p className="text-black  font-bold pt-7">
-            Responsibility:{" "}
-            <span className="font-normal">Systems Engineer</span>
+          <p className='pt-7  font-bold text-black'>
+            Responsibility:{' '}
+            <span className='font-normal'>Systems Engineer</span>
           </p>
-          <p className="text-black font-bold pt-7">
-            Experience: <span className="font-normal">18 Years</span>
+          <p className='pt-7 font-bold text-black'>
+            Experience: <span className='font-normal'>18 Years</span>
           </p>
-          <p className="text-black font-bold pt-7">
-            Email: <span className="font-normal">August@example.com</span>
+          <p className='pt-7 font-bold text-black'>
+            Email: <span className='font-normal'>August@example.com</span>
           </p>
-          <p className="text-black font-bold pt-7">
-            Phone: <span className="font-normal">+91 590 0574 258</span>
+          <p className='pt-7 font-bold text-black'>
+            Phone: <span className='font-normal'>+91 590 0574 258</span>
           </p>
-          <h2 className="text-xl font-semibold text-black pt-5">
+          <h2 className='pt-5 text-xl font-semibold text-black'>
             Social Media
           </h2>
-          <div className="flex  gap-3 pt-3">
-            <div className="group border border-gray-500 rounded bg-white p-2 hover:bg-[#0044EB]">
-              <Facebook className="text-gray-500 group-hover:text-white" />
+          <div className='flex  gap-3 pt-3'>
+            <div className='group rounded border border-gray-500 bg-white p-2 hover:bg-[#0044EB]'>
+              <Facebook className='text-gray-500 group-hover:text-white' />
             </div>
-            <div className="group rounded border border-gray-500 bg-white p-2 hover:bg-[#0044EB]">
-              <Twitter className="text-gray-500 group-hover:text-white" />
+            <div className='group rounded border border-gray-500 bg-white p-2 hover:bg-[#0044EB]'>
+              <Twitter className='text-gray-500 group-hover:text-white' />
             </div>
-            <div className="group rounded border border-gray-500 bg-white p-2 hover:bg-[#0044EB]">
-              <Linkedin className="text-gray-500 group-hover:text-white" />
+            <div className='group rounded border border-gray-500 bg-white p-2 hover:bg-[#0044EB]'>
+              <Linkedin className='text-gray-500 group-hover:text-white' />
             </div>
-            <div className="group rounded border border-gray-500 bg-white p-2 transition-all duration-300 ease-linear hover:bg-[#0044EB]">
-              <Instagram className="text-gray-500 group-hover:text-white" />
+            <div className='group rounded border border-gray-500 bg-white p-2 transition-all duration-300 ease-linear hover:bg-[#0044EB]'>
+              <Instagram className='text-gray-500 group-hover:text-white' />
             </div>
           </div>
         </div>
       </div>
       <div>
-        <h2 className="text-4xl font-bold pt-20">Professional Skills</h2>
-        <p className="pt-5 leading-8 text-lg font-semibold text-[#49515B]">
-          {" "}
+        <h2 className='pt-20 text-4xl font-bold'>Professional Skills</h2>
+        <p className='pt-5 text-lg font-semibold leading-8 text-[#49515B]'>
+          {' '}
           Since joining Techco in 1993, Matilda has been instrumental in
           transforming the company from a collection of tech-savvy individuals
           collaborating with startups to a world-renowned leader in Digital
@@ -62,52 +61,74 @@ const TeamDetail = () => {
           cultivation of a vibrant company culture that champions employee
           engagement and fosters a spirit of innovation at every level.
         </p>
-        <div className="grid grid-cols-4 mt-10 gap-5">
-          <div className="rounded-3xl h-[250px] flex flex-col justify-center px-5 py-10 bg-white relative text-center">
-            <h2 className="text-6xl font-bold">98%</h2>
-            <p className="text-xl font-semibold text-[#49515B] pt-4">
+        <div className='mt-10 grid grid-cols-4 gap-5'>
+          <div className='relative flex h-[250px] flex-col justify-center rounded-3xl bg-white px-5 py-10 text-center'>
+            <h2 className='text-6xl font-bold'>98%</h2>
+            <p className='pt-4 text-xl font-semibold text-[#49515B]'>
               Product Development
             </p>
-            <div className="w-[55%] h-1 bg-[#2144EB] absolute bottom-0 left-1/2 transform -translate-x-1/2"></div>
+            <div className='absolute bottom-0 left-1/2 h-1 w-[55%] -translate-x-1/2 bg-[#2144EB]'></div>
           </div>
-          <div className="rounded-3xl px-5 py-10 flex flex-col justify-center bg-white relative text-center">
-            <h2 className="text-6xl font-bold">88%</h2>
-            <p className="text-xl font-semibold text-[#49515B] pt-4">
+          <div className='relative flex flex-col justify-center rounded-3xl bg-white px-5 py-10 text-center'>
+            <h2 className='text-6xl font-bold'>88%</h2>
+            <p className='pt-4 text-xl font-semibold text-[#49515B]'>
               Problem-Solving
             </p>
-            <div className="w-[55%] h-1 bg-[#F26F4D] absolute bottom-0 left-1/2 transform -translate-x-1/2"></div>
+            <div className='absolute bottom-0 left-1/2 h-1 w-[55%] -translate-x-1/2 bg-[#F26F4D]'></div>
           </div>
-          <div className="rounded-3xl px-5 py-10 flex flex-col justify-center bg-white relative text-center">
-            <h2 className="text-6xl font-bold">99%</h2>
-            <p className="text-xl font-semibold text-[#49515B] pt-4">
+          <div className='relative flex flex-col justify-center rounded-3xl bg-white px-5 py-10 text-center'>
+            <h2 className='text-6xl font-bold'>99%</h2>
+            <p className='pt-4 text-xl font-semibold text-[#49515B]'>
               Communication Skills
             </p>
-            <div className="w-[55%] h-1 bg-[#F44380] absolute bottom-0 left-1/2 transform -translate-x-1/2"></div>
+            <div className='absolute bottom-0 left-1/2 h-1 w-[55%] -translate-x-1/2 bg-[#F44380]'></div>
           </div>
-          <div className="rounded-3xl px-5 py-10 flex flex-col justify-center bg-white relative text-center">
-            <h2 className="text-6xl font-bold">77%</h2>
-            <p className="text-xl font-semibold text-[#49515B] pt-4">
+          <div className='relative flex flex-col justify-center rounded-3xl bg-white px-5 py-10 text-center'>
+            <h2 className='text-6xl font-bold'>77%</h2>
+            <p className='pt-4 text-xl font-semibold text-[#49515B]'>
               Passion for Helping
             </p>
-            <div className="w-[55%] h-1 bg-[#F3A338] absolute bottom-0 left-1/2 transform -translate-x-1/2"></div>
+            <div className='absolute bottom-0 left-1/2 h-1 w-[55%] -translate-x-1/2 bg-[#F3A338]'></div>
           </div>
-              </div>
-              <h2 className="text-4xl font-bold pt-20">Educational Experience</h2>
-               <p className="pt-5 leading-8 text-lg font-semibold text-[#49515B]">
-          Armed with a Bachelor's degree in Computer Science from the University of XYZ, I specialized in data structures, algorithms, and networks. Expanding my knowledge, I pursued a Master's in Information Technology Management at ABC University.
-              </p>
-               <div className=" p-4 rounded-lg">
-      <h2 className="text-lg font-bold mb-4">Qualifications:</h2>
-      <ul className="list-disc pl-5 space-y-2 text-xl leading-8  text-[#49515B]" style={{ listStyleType: "disc" }}>
-        <li className="marker:text-[#0044EB]">Proficiency in systems analysis, design, implementation, and maintenance.</li>
-        <li className="marker:text-[#0044EB]">Strong knowledge of network protocols, hardware, and software components.</li>
-        <li className="marker:text-[#0044EB]">Experience with virtualization technologies (VMware, Hyper-V).</li>
-        <li className="marker:text-[#0044EB]">Skilled in cloud computing platforms (AWS, Azure, Google Cloud).</li>
-        <li className="marker:text-[#0044EB]">Proficient in scripting languages (Python, PowerShell).</li>
-        <li className="marker:text-[#0044EB]">Experience in system security and disaster recovery planning.</li>
-        <li className="marker:text-[#0044EB]">Excellent problem-solving and analytical skills.</li>
-      </ul>
-    </div>
+        </div>
+        <h2 className='pt-20 text-4xl font-bold'>Educational Experience</h2>
+        <p className='pt-5 text-lg font-semibold leading-8 text-[#49515B]'>
+          Armed with a Bachelor's degree in Computer Science from the University
+          of XYZ, I specialized in data structures, algorithms, and networks.
+          Expanding my knowledge, I pursued a Master's in Information Technology
+          Management at ABC University.
+        </p>
+        <div className=' rounded-lg p-4'>
+          <h2 className='mb-4 text-lg font-bold'>Qualifications:</h2>
+          <ul
+            className='list-disc space-y-2 pl-5 text-xl leading-8  text-[#49515B]'
+            style={{ listStyleType: 'disc' }}
+          >
+            <li className='marker:text-[#0044EB]'>
+              Proficiency in systems analysis, design, implementation, and
+              maintenance.
+            </li>
+            <li className='marker:text-[#0044EB]'>
+              Strong knowledge of network protocols, hardware, and software
+              components.
+            </li>
+            <li className='marker:text-[#0044EB]'>
+              Experience with virtualization technologies (VMware, Hyper-V).
+            </li>
+            <li className='marker:text-[#0044EB]'>
+              Skilled in cloud computing platforms (AWS, Azure, Google Cloud).
+            </li>
+            <li className='marker:text-[#0044EB]'>
+              Proficient in scripting languages (Python, PowerShell).
+            </li>
+            <li className='marker:text-[#0044EB]'>
+              Experience in system security and disaster recovery planning.
+            </li>
+            <li className='marker:text-[#0044EB]'>
+              Excellent problem-solving and analytical skills.
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
