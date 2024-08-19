69906324a0568deabd642dc93507925df07fee7e
Iqbal Hassan <iqbal.hassan2584@gmail.com>
2024-08-19 13:07:37 +0500

    fix logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,20 +1,22 @@
 import { forwardRef } from 'react';
 import { Link } from 'react-router-dom';
-import { APP_TITLE } from '@src/constants/variables';
-// import { Image } from '@src/components/ui/image/Image';
+// import { APP_TITLE } from '@src/constants/variables';
+import { Image } from '@src/components/ui/image/Image';
 // import logoIcon from '@src/assets/images/logo/logo-icon.png';
+// import logoBlue from '@src/assets/images/logo/logo-blue.png';
+import logoWhite from '@src/assets/images/logo/logo-white.png';
 
 export const Logo = forwardRef(() => {
   return (
     <Link to='/' className='flex items-center justify-center gap-0.5 lg:gap-2'>
-      {/* <Image
-        src={logoIcon}
+      <Image
+        src={logoWhite}
         alt='Awal-solution-logo-icon'
-        className='w-8 lg:w-10'
-      /> */}
-      <h1 className='font-sans text-2xl font-semibold capitalize text-white'>
+        className='h-16 w-24'
+      />
+      {/* <h1 className='font-sans text-2xl font-semibold capitalize text-white'>
         {APP_TITLE}
-      </h1>
+      </h1> */}
     </Link>
   );
 });
